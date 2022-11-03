2a04208804a414d2525a143ba37a592668e4eb30
GouriJawale <117344940+GouriJawale@users.noreply.github.com>
2022-11-03 13:48:30 +0530

    Add files via upload

---
+++ PositisveNegative.java
@@ -0,0 +1,24 @@
+package basicprogram;
+// Program to check that number is positive or negative.
+import java.util.*;
+public class PositisveNegative {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = sc.nextInt();
+		if(a>0) {
+			System.out.println("number is positive");
+		}
+		else {
+			if(a<0) {
+				System.out.println("number is negative");
+				
+			}
+			else {
+				System.out.println("You entered Zero");
+			}
+	}
+		
+	}
+
+}
